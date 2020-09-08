d310cb154656019b9bd8f44f3c9c66fd7e81e934
Isabela <isabelasilva@touchtec.com.br>
2020-09-07 21:53:28 -0300

    inserção de regras e cores

--- app/src/main/java/com/example/atv_app_isabela/MainActivity.kt
+++ app/src/main/java/com/example/atv_app_isabela/MainActivity.kt
@@ -15,26 +15,30 @@ class MainActivity : AppCompatActivity() {
 
     fun botao(componente: View){
 
-        tv_final.setTextColor(Color.MAGENTA)
-        tv_final.text = "MUDANDO A FRASEEEE !!!"
-
+        val nome = et_primeiro.text.toString()
         val salario = et_segundo.text.toString().toInt()
         val gasto = et_terceiro.text.toString().toInt()
         val total = salario - gasto
 
-        if(salario < 100){
+        if(nome.length < 3){
+            tv_final.setTextColor(Color.RED)
+            tv_final.text = "Nome inválido, por favor digite um nome com mais de 3 ou maos caracteres"
+        } else if(salario < 100){
+            tv_final.setTextColor(Color.RED)
             tv_final.text = "É necessário ter uma renda maior que 100 reais"
         }else if (gasto < 30){
+            tv_final.setTextColor(Color.RED)
             tv_final.text = "É necessário ter um gasto maior que 30 reais para poder calcular"
         } else {
             if (salario > gasto) {
-                tv_final.text = "Ainda resta ${total} para gastar, " +
+                tv_final.setTextColor(Color.GREEN)
+                tv_final.text = "${nome}, ainda resta ${total} para você gastar, " +
                         "se você guardar todo mês esse dinheiro durante 1 ano vai sobrar $ ${total * 12} reais"
-//        }else if (gasto > salario){
-//            tv_final.text = "Valor inválido, você não pode gastar mais do que você ganha"
+
             } else {
-                tv_final.text = "Valor inválido, você tem ${total} e não pode gastar " +
-                        "mais do que você ganha. VC QUER FALIR ???"
+                tv_final.setTextColor(Color.RED)
+                tv_final.text = "${nome}, você gastou R$ ${gasto - salario} a mais do que você ganha" +
+                        " VC QUER FALIR ???"
             }
         }
 
